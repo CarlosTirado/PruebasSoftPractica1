221a385f3afd7b89fdf39989d541f38e1dfde996
Samir Miranda <Bya_smiranda@outlook.com>
2021-08-21 08:45:55 -0500

    se confirma clase con pruebas unitarias visualizando la ejecución de los 4 casos de prueba propuestos

---
+++ Practica1Test/DataTestTeoremaDesigualdadTriangulo.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practica1;
+using System;
+
+namespace Practica1Test
+{
+    [TestClass]
+    public class DataTestTeoremaDesigualdadTriangulo
+    {
+        [DataTestMethod]
+        [DataRow(3, 4, 5, true)]
+        [DataRow(5, 12, 13, true)]
+        [DataRow(5, 7, 13, false)]
+        [DataRow(5, 9, 3, false)]
+        public void Test(
+            int ladoA,
+            int ladoB,
+            int ladoC,
+            bool resultadoEsperado)
+        {
+            var servicio = new VerificarTeoremaDesigualdadTrianguloService();
+            var response = servicio.Verificar(
+                ladoA,
+                ladoB,
+                ladoC);
+
+            Assert.AreEqual(response, resultadoEsperado);
+        }
+    }
+}
--- Practica1Test/TestTeoremaDesigualdadTriangulo.cs
+++ Practica1Test/TestTeoremaDesigualdadTriangulo.cs
@@ -7,24 +7,68 @@ namespace Practica1Test
     [TestClass]
     public class TestTeoremaDesigualdadTriangulo
     {
-        [DataTestMethod]
-        [DataRow(3, 4, 5, true)]
-        [DataRow(5, 12, 13, true)]
-        [DataRow(5, 7, 13, false)]
-        [DataRow(5, 9, 3, false)]
-        public void Test(
-            int ladoA,
-            int ladoB,
-            int ladoC,
-            bool resultadoEsperado)
+        [TestMethod]
+        public void TestCase1Triangulo_3_4_5_True()
         {
+            int ladoA = 3,
+                ladoB = 4,
+                ladoC = 5;
+
             var servicio = new VerificarTeoremaDesigualdadTrianguloService();
             var response = servicio.Verificar(
                 ladoA,
                 ladoB,
                 ladoC);
 
-            Assert.AreEqual(response, resultadoEsperado);
+            Assert.AreEqual(true, response);
+        }
+
+        [TestMethod]
+        public void TestCase2Triangulo_5_12_13_True()
+        {
+            int ladoA = 5,
+                ladoB = 12,
+                ladoC = 13;
+
+            var servicio = new VerificarTeoremaDesigualdadTrianguloService();
+            var response = servicio.Verificar(
+                ladoA,
+                ladoB,
+                ladoC);
+
+            Assert.AreEqual(true, response);
+        }
+
+        [TestMethod]
+        public void TestCase3Triangulo_5_7_13_False()
+        {
+            int ladoA = 5,
+                ladoB = 7,
+                ladoC = 13;
+
+            var servicio = new VerificarTeoremaDesigualdadTrianguloService();
+            var response = servicio.Verificar(
+                ladoA,
+                ladoB,
+                ladoC);
+
+            Assert.AreEqual(false, response);
+        }
+
+        [TestMethod]
+        public void TestCase4Triangulo_5_9_3_False()
+        {
+            int ladoA = 5,
+                ladoB = 9,
+                ladoC = 3;
+
+            var servicio = new VerificarTeoremaDesigualdadTrianguloService();
+            var response = servicio.Verificar(
+                ladoA,
+                ladoB,
+                ladoC);
+
+            Assert.AreEqual(false, response);
         }
     }
 }
