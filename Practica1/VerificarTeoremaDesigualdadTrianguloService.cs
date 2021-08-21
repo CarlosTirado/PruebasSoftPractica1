a2c0e94f41641fe119b261932e2c53b4f2d3c903
CarlosTirado <orley333@hotmail.com>
2021-08-21 08:19:09 -0500

    Init

---
+++ Practica1/VerificarTeoremaDesigualdadTrianguloService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practica1
+{
+    public class VerificarTeoremaDesigualdadTrianguloService
+    {
+        public bool Verificar(
+            int ladoA,
+            int ladoB,
+            int ladoC)
+        {
+            return 
+                (ladoA + ladoB > ladoC) &&
+                (ladoA + ladoC > ladoB) &&
+                (ladoB + ladoC > ladoA);
+        }
+    }
+}
